ef9a6c4b7f05e35afd374bf5e3346a08188d3458
tomasote572 <tomas.ramirez@mail.escuelaing.edu.co>
2025-11-13 18:34:13 -0500

    Organizacion completa

---
+++ src/main/java/ECIEXPRESS/Amaterasu_Pagos/Promotion/_BackEnd/Amaterasu_Pagos/Promotion/_BackEnd/Exception/Excepciones.java
@@ -0,0 +1,4 @@
+package ECIEXPRESS.Amaterasu_Pagos.Promotion._BackEnd.Amaterasu_Pagos.Promotion._BackEnd.Exception;
+
+public class Excepciones {
+}
---
+++ src/main/java/ECIEXPRESS/Amaterasu_Pagos/Promotion/_BackEnd/Amaterasu_Pagos/Promotion/_BackEnd/Infrastructure/Persistence/ClasesMongoRepository.java
@@ -0,0 +1,4 @@
+package ECIEXPRESS.Amaterasu_Pagos.Promotion._BackEnd.Amaterasu_Pagos.Promotion._BackEnd.Infrastructure.Persistence;
+
+public class ClasesMongoRepository {
+}
